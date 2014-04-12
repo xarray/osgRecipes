3aa13bc9c438d47f9b33217c67ecbc14bcbf311c
xarray <wangray84@gmail.com>
2014-04-12 22:04:28 +0800

    From Émeric MASCHINO: fix to chapter9/osg_qt example

--- cookbook/chapter9/ch09_01/osg_qt.cpp
+++ cookbook/chapter9/ch09_01/osg_qt.cpp
@@ -1,6 +1,6 @@
 /* -*-c++-*- OpenSceneGraph Cookbook
- * Chapter 9 Recipe 1
- * Author: Wang Rui <wangray84 at gmail dot com>
+* Chapter 9 Recipe 1
+* Author: Wang Rui <wangray84 at gmail dot com>
 */
 
 #include <QtCore/QtCore>
@@ -13,9 +13,8 @@
 
 #include "CommonFunctions"
 
-osg::Camera* createCamera( int x, int y, int w, int h )
+osgQt::GraphicsWindowQt* createGraphicsWindow( int x, int y, int w, int h )
 {
-    osg::DisplaySettings* ds = osg::DisplaySettings::instance().get();
     osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
     traits->windowDecoration = false;
     traits->x = x;
@@ -23,44 +22,42 @@ osg::Camera* createCamera( int x, int y, int w, int h )
     traits->width = w;
     traits->height = h;
     traits->doubleBuffer = true;
-    
-    osg::ref_ptr<osg::Camera> camera = new osg::Camera;
-    camera->setGraphicsContext( new osgQt::GraphicsWindowQt(traits.get()) );
-    camera->setClearColor( osg::Vec4(0.2, 0.2, 0.6, 1.0) );
-    camera->setViewport( new osg::Viewport(0, 0, traits->width, traits->height) );
-    camera->setProjectionMatrixAsPerspective(
-        30.0f, static_cast<double>(traits->width)/static_cast<double>(traits->height), 1.0f, 10000.0f );
-    return camera.release();
+
+    return new osgQt::GraphicsWindowQt(traits.get());
 }
 
 class ViewerWidget : public QWidget
 {
 public:
-    ViewerWidget( osg::Camera* camera, osg::Node* scene )
-    :   QWidget()
+    ViewerWidget( osgQt::GraphicsWindowQt* gw, osg::Node* scene )
+    : QWidget()
     {
-        _viewer.setCamera( camera );
+        const osg::GraphicsContext::Traits* traits = gw->getTraits();
+
+        osg::Camera* camera = _viewer.getCamera();
+        camera->setGraphicsContext( gw );
+        camera->setClearColor( osg::Vec4(0.2, 0.2, 0.6, 1.0) );
+        camera->setViewport( new osg::Viewport(0, 0, traits->width, traits->height) );
+        camera->setProjectionMatrixAsPerspective(
+            30.0f, static_cast<double>(traits->width)/static_cast<double>(traits->height), 1.0f, 10000.0f );
+
         _viewer.setSceneData( scene );
         _viewer.addEventHandler( new osgViewer::StatsHandler );
         _viewer.setCameraManipulator( new osgGA::TrackballManipulator );
         _viewer.setThreadingModel( osgViewer::Viewer::SingleThreaded );
-        
-        osgQt::GraphicsWindowQt* gw = dynamic_cast<osgQt::GraphicsWindowQt*>( camera->getGraphicsContext() );
-        if ( gw )
-        {
-            QVBoxLayout* layout = new QVBoxLayout;
-            layout->addWidget( gw->getGLWidget() );
-            setLayout( layout );
-        }
-        
+
+        QVBoxLayout* layout = new QVBoxLayout;
+        layout->addWidget( gw->getGLWidget() );
+        setLayout( layout );
+
         connect( &_timer, SIGNAL(timeout()), this, SLOT(update()) );
         _timer.start( 40 );
     }
-    
+
 protected:
     virtual void paintEvent( QPaintEvent* event )
     { _viewer.frame(); }
-    
+
     osgViewer::Viewer _viewer;
     QTimer _timer;
 };
@@ -68,10 +65,10 @@ protected:
 int main( int argc, char** argv )
 {
     QApplication app( argc, argv );
-    osg::Camera* camera = createCamera( 50, 50, 640, 480 );
+    osgQt::GraphicsWindowQt* gw = createGraphicsWindow( 50, 50, 640, 480 );
     osg::Node* scene = osgDB::readNodeFile("cow.osg");
-    
-    ViewerWidget* widget = new ViewerWidget(camera, scene);
+
+    ViewerWidget* widget = new ViewerWidget(gw, scene);
     widget->setGeometry( 100, 100, 800, 600 );
     widget->show();
     return app.exec();
